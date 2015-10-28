aa5af16c399115088e9ec72cc626aa250cfedc00
oakland <jizhengqistrongman@126.com>
2015-10-28 20:17:47 +0800

    5th week task1

---
+++ 5th week/task1-分数分界/score-division.js
@@ -0,0 +1,21 @@
+function classify() {
+    var score = document.getElementById("score");
+    var scoreinput = parseInt(score.value);
+    var result = document.getElementById("result");
+    switch (true) {
+        case (scoreinput == null):
+            alert("请输入数字1-100");
+            break;
+        case (typeof scoreinput !== Number):
+            alert("请输入1-100的数字");
+            break;
+        case (scoreinput < 0 || scoreinput > 100):
+            alert("请输入1-100的数字");
+            break;
+        case (scoreinput == 100):
+            result.innerHTML = "恭喜，获得满分！";
+            break;
+        default:
+            result.innerHTML = (10 - parseInt(scoreinput / 10)) + "等生";
+    }
+}
